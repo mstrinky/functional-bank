0fbbb971a12b21af69094dca9afbe47baa75fd43
Mst Rinky Akter <mstrinkya@gmail.com>
2022-10-20 19:14:35 +0600

    deposit withdraw tatal balance added

--- js/banking.js
+++ js/banking.js
@@ -1,15 +1,58 @@
 document.getElementById('btn-deposit').addEventListener('click',function(){
    const depositFiled = document.getElementById('deposit-field');
+
    const depositAmount = depositFiled.value ;
+    const presentDepositAmount = parseFloat(depositAmount);
+
 //    console.log(depositAmount); 
 
 //    get current deposit 
 
 const depositTotal = document.getElementById('deposit-total');
+
 const depositTotalText = depositTotal.innerText;
-depositTotal.innerText = depositAmount;
+const previousDepositAmount =parseFloat(depositTotalText);
+
+depositTotal.innerText= presentDepositAmount + previousDepositAmount;
+
+// depositTotal.innerText = depositAmount;
 // console.log(depositTotalText);
+
+// update balance 
+const balanceTotal= document.getElementById('balance-total');
+ const balanceTotalText= balanceTotal.innerText;   
+ //  .innerText নিলাম কারণ ্ এটা span  বা h4 আর্থ্্যা ং text এর মধ্যে আছে । যদি input এর মধ্যে থাকতো তাহলে .value নিতাম ।
+const previousBalanceTotal = parseFloat(balanceTotalText);
+balanceTotal.innerText =  previousBalanceTotal + presentDepositAmount ;
 
 // clear deposit field 
 depositFiled.value = '';
+})
+
+// handle withdraw button 
+
+document.getElementById('btn-withdraw').addEventListener('click',function(){
+   // console.log('clicked'); 
+   const withdrawField = document.getElementById('withdraw-field');
+
+   withdrawAmountText = withdrawField.value;
+   const withdrawAmount = parseFloat(withdrawAmountText);
+   // input এর ভিতরে যে লেখাটা আছে সেটাকে ধরার জন্য .value use করব ।
+   // console.log('withdrawField');
+
+   // update withdraw total 
+
+   const withdrawTotal = document.getElementById('withdraw-total');
+   const withdrawTotalText = withdrawTotal.innerText;
+   const previousWithdrawAmount = parseFloat(withdrawTotalText);
+     withdrawTotal.innerText = previousWithdrawAmount +  withdrawAmount ;
+   //   update balance after withdraw 
+const balanceTotal = document.getElementById('balance-total');
+const balanceTotalText = balanceTotal.innerText;
+const previousBalanceTotal = parseFloat(balanceTotalText);
+ balanceTotal.innerText = previousBalanceTotal - withdrawAmount;
+
+
+   // clear withdraw input field 
+   withdrawField.value ='';
 })
